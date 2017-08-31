97415548a9984d3e6c21deda30501b454b72872c
IceTesla <877790251@qq.com>
2017-08-31 16:03:41 +0800

    实现购物车功能UI

--- app/src/main/java/cn/IceTesla/easyorder/Adapter/ShopcartAdapter.java
+++ app/src/main/java/cn/IceTesla/easyorder/Adapter/ShopcartAdapter.java
@@ -1,8 +1,20 @@
 package cn.IceTesla.easyorder.Adapter;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.IceTesla.easyorder.Data.Model.ShopcartModel;
+import cn.IceTesla.easyorder.R;
 
 /**
  * Created by IceTesla on 2017/8/30.
@@ -10,14 +22,31 @@ import android.widget.BaseAdapter;
 
 public class ShopcartAdapter extends BaseAdapter
 {
-    @Override
-    public int getCount() {
-        return 0;
+    private List<ShopcartModel> shopcartList;
+    private Activity activity;
+    private LayoutInflater layoutInflater;
+    private ViewHolder viewHolder;
+
+    public ShopcartAdapter(Activity activity, List<ShopcartModel> list){
+        this.shopcartList = list;
+        this.activity = activity;
+    }
+
+    public void add(List<ShopcartModel> list){
+        this.shopcartList.addAll(list);
+    }
+    public void refresh(List<ShopcartModel> list) {
+        this.shopcartList = list;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public int getCount() {
+        return shopcartList.size();
+    }
+
+    @Override
+    public ShopcartModel getItem(int position) {
+        return shopcartList.get(position);
     }
 
     @Override
@@ -27,6 +56,60 @@ public class ShopcartAdapter extends BaseAdapter
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if(convertView ==null){
+            layoutInflater = LayoutInflater.from(activity);
+            convertView = layoutInflater.inflate(R.layout.item_shopcart_layout, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        ShopcartModel item = getItem(position);
+        viewHolder.setValues(item);
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        ImageView image;
+        TextView name;
+        TextView sale;
+        TextView good;
+        TextView price;
+        TextView number;
+        LinearLayout singleItem;
+
+        public ViewHolder(View view){
+            image = (ImageView) view.findViewById(R.id.image_item_shopcart);
+            name = (TextView) view.findViewById(R.id.text_item_shopcart_name);
+            sale = (TextView) view.findViewById(R.id.text_item_shopcart_sale);
+            good = (TextView) view.findViewById(R.id.text_item_shopcart_good);
+            price = (TextView) view.findViewById(R.id.text_item_shopcart_price);
+            number = (TextView) view.findViewById(R.id.text_item_shopcart_number);
+            singleItem = (LinearLayout) view.findViewById(R.id.item_shopcart);
+        }
+
+        public void setValues(final ShopcartModel item){
+            Resources res=activity.getResources();
+            name.setText(item.getName());
+            sale.setText(item.getSale());
+            good.setText(item.getGood());
+            price.setText(item.getPrice());
+            number.setText(item.getNumber());
+
+            ArrayList<String> picList = item.getImage();
+
+            singleItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            if (!picList.isEmpty()){
+
+            }
+        }
     }
 }
--- app/src/main/java/cn/IceTesla/easyorder/Data/Model/ShopcartModel.java
+++ app/src/main/java/cn/IceTesla/easyorder/Data/Model/ShopcartModel.java
@@ -1,10 +1,65 @@
 package cn.IceTesla.easyorder.Data.Model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by IceTesla on 2017/8/30.
  */
 
 public class ShopcartModel implements Serializable {
+    private String name;
+    private ArrayList image;
+    private int sale;
+    private int good;
+    private int price;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList getImage() {
+        return image;
+    }
+
+    public void setImage(ArrayList image) {
+        this.image = image;
+    }
+
+    public int getSale() {
+        return sale;
+    }
+
+    public void setSale(int sale) {
+        this.sale = sale;
+    }
+
+    public int getGood() {
+        return good;
+    }
+
+    public void setGood(int good) {
+        this.good = good;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
